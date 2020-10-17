3acfe32d6f48c6ac8081b99925925af89059b12c
Karen <kngs@ic.ufal.br>
2020-10-16 21:59:31 -0300

    feat: create backend structure

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json())
+
+app.post("/users/:id", (request, response) => {
+  console.log(request.query);
+  console.log(request.query);
+  console.log(request.body);
+
+  return response.json({ message: "Hello world" });
+});
+
+app.listen(3333);
