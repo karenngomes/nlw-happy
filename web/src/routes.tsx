1e4f248ca05ad93986938d37a7479e0ccfb95f29
Karen <kngs@ic.ufal.br>
2020-10-18 12:36:44 -0300

    feat(web): add create orphanage page, route and styles

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,16 +3,18 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import OrphanagesMap from "./pages/OrphanagesMap";
+import CreateOrphanage from "./pages/CreateOrphanage";
 
 function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Landing} />
-                <Route path="/app" exact component={OrphanagesMap} />
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/app" exact component={OrphanagesMap} />
+        <Route path="/orphanages/create" exact component={CreateOrphanage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default Routes;
