b157cd2e912fced0b7b6a1496a00747d125c22bd
Karen <kngs@ic.ufal.br>
2020-10-18 04:36:28 -0300

    feat(backend): create orphanage route connecting with database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,39 @@
-import express from "express";
+import express, { request } from "express";
+import { getRepository } from "typeorm";
+import Orphanage from "./models/Orphanage";
+
 import "./database/connection";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/users/:id", (request, response) => {
-  console.log(request.query);
-  console.log(request.query);
-  console.log(request.body);
+app.post("/orphanages", async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
 
-  return response.json({ message: "Hello world" });
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.status(201).json(orphanage);
 });
 
 app.listen(3333);
